6d74a7da88dfe3cc6a02fdde72b4ce9797650f15
diogo <diogo.lima.flooxai@gmail.com>
2025-04-10 10:07:13 -0300

    feat: add fragment and ferfactoring the code to the app component using arrow function

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,15 +1,11 @@
+import App from "./App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
-import Primeiro from "./components/basicos/Primeiro";
-import ComParametro from "./components/basicos/ComParametro";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <div id="app">
-    <Primeiro></Primeiro>
-    <ComParametro titulo="Situação do Aluno" aluno="Maguin" nota={9.3} />
-  </div>
+  <App />
 );
